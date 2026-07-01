1ada9b4e749e959478fc39606d9ebfffbbe50b33
Alberto Ubals <ubals.alberto@gmail.com>
2026-07-01 16:30:40 -0500

    add date field to pr model and migrate

--- backend/app/models/pr.py
+++ backend/app/models/pr.py
@@ -1,8 +1,10 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import datetime, timezone
 
 class Pr(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     set_id: int = Field(foreign_key="workoutset.id")
     user_id: int = Field(foreign_key="user.id")
     exercise_def_id: int = Field(foreign_key="exercisedefinition.id")
+    date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
